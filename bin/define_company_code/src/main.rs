1418118e3094d15d46a08c46de55378ffcc69fe1
root <root@DESKTOP-NUK9MF0.localdomain>
2022-08-22 18:04:50 +0530

    Added company code

--- bin/define_company_code/src/main.rs
+++ bin/define_company_code/src/main.rs
@@ -8,43 +8,45 @@ use uuid::Uuid;
 #[derive(Debug, Default, Deserialize, Clone)]
 struct CompanyCode {
     #[serde(default)]
-    pub company_code_pk: Uuid,
+    company_code: String,
     #[serde(default)]
-    pub company_code: String,
+    company_name: String,
     #[serde(default)]
-    pub company_name: String,
+    city: String,
     #[serde(default)]
-    pub city: String,
+    country: String,
     #[serde(default)]
-    pub country: String,
+    language: String,
     #[serde(default)]
-    pub language: String,
+    currency: String,
     #[serde(default)]
-    pub currency: String,
+    title: String,
     #[serde(default)]
-    pub search_terms: String,
+    search_terms: Vec<String>,
     #[serde(default)]
-    pub street_house_no: String,
+    street_house_no: String,
     #[serde(default)]
-    pub postal_code: String,
+    postal_code: String,
     #[serde(default)]
-    pub telephone: Option<u64>,
+    region: String,
     #[serde(default)]
-    pub tel_ext: Option<u16>,
+    telephone: Option<String>,
     #[serde(default)]
-    pub mobile_phone: Option<u64>,
+    tel_ext: Option<String>,
     #[serde(default)]
-    pub fax: Option<u64>,
+    fax: Option<String>,
     #[serde(default)]
-    pub fax_ext: Option<u16>,
+    fax_ext: Option<String>,
     #[serde(default)]
-    pub email: String,
+    mobile_phone: Option<String>,
     #[serde(default)]
-    pub comments: Option<String>,
+    email: String,
     #[serde(default)]
-    pub request: Option<String>,
+    comments: Option<String>,
     #[serde(default)]
-    pub short_desc: Option<String>,
+    request: Option<String>,
+    #[serde(default)]
+    short_desc: Option<String>
 }
 
 #[tokio::main]
@@ -64,37 +66,40 @@ async fn define_company_code(
 ) -> Result<Response<Body>, CustomError> {
     let new_company_code: Option<CompanyCode> = request.payload().unwrap();
 
-    let company_code_pk_av = AttributeValue::S(Uuid::new_v4().to_string());
+    let company_pk_av = AttributeValue::S(Uuid::new_v4().to_string());
     let company_code_av = AttributeValue::S(new_company_code.clone().unwrap().company_code.into());
     let company_name_av = AttributeValue::S(new_company_code.clone().unwrap().company_name.into());
     let city_av = AttributeValue::S(new_company_code.clone().unwrap().city.into());
     let country_av = AttributeValue::S(new_company_code.clone().unwrap().country.into());
     let language_av = AttributeValue::S(new_company_code.clone().unwrap().language.into());
     let currency_av = AttributeValue::S(new_company_code.clone().unwrap().currency.into());
-    let search_terms_av = AttributeValue::S(new_company_code.clone().unwrap().search_terms.into());
+    let title_av = AttributeValue::S(new_company_code.clone().unwrap().title.into());
+    let search_terms_av = AttributeValue::Ss(new_company_code.clone().unwrap().search_terms.into());
     let street_house_no_av = AttributeValue::S(new_company_code.clone().unwrap().street_house_no.into());
     let postal_code_av = AttributeValue::S(new_company_code.clone().unwrap().postal_code.into());
-    let telephone_av = match new_company_code.clone().unwrap().telephone.unwrap().to_string().len() {
+    let telephone_av = match new_company_code.clone().unwrap().telephone.unwrap().len() {
         0 => AttributeValue::Null(true),
-        _ => AttributeValue::N(new_company_code.clone().unwrap().telephone.unwrap().to_string())
+        _ => AttributeValue::S(new_company_code.clone().unwrap().telephone.unwrap().into())
     };
-    let tel_ext_av = match new_company_code.clone().unwrap().tel_ext.unwrap().to_string().len() {
+    let tel_ext_av = match new_company_code.clone().unwrap().tel_ext.unwrap().len() {
         0 => AttributeValue::Null(true),
-        _ => AttributeValue::N(new_company_code.clone().unwrap().tel_ext.unwrap().to_string())
+        _ => AttributeValue::S(new_company_code.clone().unwrap().tel_ext.unwrap().into())
     };
-    let mobile_phone_av = match new_company_code.clone().unwrap().mobile_phone.unwrap().to_string().len() {
+    let fax_av = match new_company_code.clone().unwrap().fax.unwrap().len() {
         0 => AttributeValue::Null(true),
-        _ => AttributeValue::N(new_company_code.clone().unwrap().mobile_phone.unwrap().to_string())
+        _ => AttributeValue::S(new_company_code.clone().unwrap().fax.unwrap().into())
     };
-    let fax_av = match new_company_code.clone().unwrap().fax.unwrap().to_string().len() {
+    let fax_ext_av = match new_company_code.clone().unwrap().fax_ext.unwrap().len() {
         0 => AttributeValue::Null(true),
-        _ => AttributeValue::N(new_company_code.clone().unwrap().fax.unwrap().to_string())
+        _ => AttributeValue::S(new_company_code.clone().unwrap().fax_ext.unwrap().into())
     };
-    let fax_ext_av = match new_company_code.clone().unwrap().fax_ext.unwrap().to_string().len() {
+    let mobile_phone_av = match new_company_code.clone().unwrap().mobile_phone.unwrap().len() {
         0 => AttributeValue::Null(true),
-        _ => AttributeValue::N(new_company_code.clone().unwrap().fax_ext.unwrap().to_string())
+        _ => AttributeValue::S(new_company_code.clone().unwrap().mobile_phone.unwrap().into())
     };
     let email_av = AttributeValue::S(new_company_code.clone().unwrap().email.into());
+    let postal_code_av = AttributeValue::S(new_company_code.clone().unwrap().postal_code.into());
+
     let comments_av = match new_company_code.clone().unwrap().comments.unwrap().len() {
         0 => AttributeValue::Null(true),
         _ => AttributeValue::S(new_company_code.clone().unwrap().comments.unwrap().into())
@@ -113,21 +118,22 @@ async fn define_company_code(
     let response = match client.db_client
     .put_item()
     .table_name("company_code")
-    .item("company_code_pk", company_code_pk_av)
+    .item("company_pk", company_pk_av)
     .item("company_code", company_code_av)
     .item("company_name", company_name_av)
     .item("city", city_av)
     .item("country", country_av)
     .item("language", language_av)
     .item("currency", currency_av)
+    .item("title", title_av)
     .item("search_terms", search_terms_av)
     .item("street_house_no", street_house_no_av)
     .item("postal_code", postal_code_av)
     .item("telephone", telephone_av)
     .item("tel_ext", tel_ext_av)
-    .item("mobile_phone", mobile_phone_av)
     .item("fax", fax_av)
     .item("fax_ext", fax_ext_av)
+    .item("mobile_phone", mobile_phone_av)
     .item("email", email_av)
     .item("comments", comments_av)
     .item("request", request_av)
@@ -138,12 +144,12 @@ async fn define_company_code(
         Ok(_) => Response::builder()
         .status(201)
         .header("content-type", "application/json")
-        .body(Body::from(format!("Added a new company code as {} company code", new_company_code.as_ref().unwrap().company_code)))?,
+        .body(Body::from(format!("Added a new company code as {}", new_company_code.as_ref().unwrap().company_code)))?,
         
         Err(e) => Response::builder()
         .status(400)
         .header("content-type", "application/json")
-        .body(Body::from(format!("New company code not created due to error: {:?}", e)))?
+        .body(Body::from(format!("New company not created due to error: {:?}", e)))?
     };
 
     Ok(response)
